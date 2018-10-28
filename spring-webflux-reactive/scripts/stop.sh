3796bc3d6c760266449d1ab0bd2a1ade22fa0c4f
dgaurava <44541278+dgaurava@users.noreply.github.com>
2018-10-28 15:57:38 +0530

    Create stop.sh
    aws - stop script

---
+++ spring-webflux-reactive/scripts/stop.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Stop apache service before deploying new code
+service apache2 stop
+
+#mkdir -p /home/ec2-user/spring-web-flux/revisions/$(date "+%Y%m%d%H%M%S")
+
+
+#tar -vczf /home/sap/betasite-git-backup.`date`.tar.gz /home/xxuserxx/xxcodedirectoryxx
+#Copy current code to a directory
